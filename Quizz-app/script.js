a98cdd8d3d2ec6c25e9a2c7c564eb9b3918d626a
Cristian Rodrigo Estrada Romero <ecristian963@gmail.com>
2021-09-03 13:26:02 -0500

    Logic add on script.js
    i did a modify in inde.html bc script does'nt work correctly, the issue was that 's on header

--- Quizz-app/script.js
+++ Quizz-app/script.js
@@ -28,4 +28,28 @@ const quizData = [
         d:"respuesta 4",
         correct:'a'
     }
-]
+];
+const questionEl = document.getElementById('question');
+const a_texto = document.getElementById('a_texto');
+const b_texto = document.getElementById('b_texto');
+const c_texto = document.getElementById('c_texto');
+const d_texto = document.getElementById('d_texto');
+const submitBtn = document.getElementById('submit');
+
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    
+    a_texto.innerText = currentQuizData.a;
+    b_texto.innerText = currentQuizData.b;
+    c_texto.innerText = currentQuizData.c;
+    d_texto.innerText = currentQuizData.d;
+
+    currentQuestion++;
+}
