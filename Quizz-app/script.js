f8cba7b7cfde3495bd5cc4696523a378e7a52ad9
Cristian Rodrigo Estrada Romero <ecristian963@gmail.com>
2021-09-03 12:54:28 -0500

    Quiz app integration
    + ADD index .html
    + script.js
    + style.css
    
    use gradients

---
+++ Quizz-app/script.js
@@ -0,0 +1,31 @@
+const quizData = [
+    {
+        question:"texto de prueba",
+        a:"respuesta 1",
+        b:"respuesta 2",
+        c:"respuesta 3",
+        d:"respuesta 4",
+        correct: 'c'
+    },{
+        question: "texto de prueba 2",
+        a:"respuesta 1",
+        b:"respuesta 2",
+        c:"respuesta 3",
+        d:"respuesta 4",
+        correct: 'b'
+    },{
+        question: "texto de prueba 3",
+        a:"respuesta 1",
+        b:"respuesta 2",
+        c:"respuesta 3",
+        d:"respuesta 4",
+        correct: 'd'
+    },{
+        question: "texto de prueba 4",
+        a:"respuesta 1",
+        b:"respuesta 2",
+        c:"respuesta 3",
+        d:"respuesta 4",
+        correct:'a'
+    }
+]
